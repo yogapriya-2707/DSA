fabb2e5c53128e9afebafdaee9ed29dae182cd6f
Yogapriya.G <gyogapriya2707@gmail.com>
2026-03-21 12:25:17 +0530

    Add files via upload

---
+++ IfElseIf.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static String compareNM(int n, int m) {
+        if(n < m){
+            return "lesser";
+        }
+        else if(n == m){
+            return "equal";
+        }
+        else{
+            return "greater";
+        }
+    }
+}
