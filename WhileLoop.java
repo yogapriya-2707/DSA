b050dafa8a3eb0402d8b921bad2f8a498fcd527a
Yogapriya.G <gyogapriya2707@gmail.com>
2026-03-21 13:23:27 +0530

    Add files via upload

---
+++ WhileLoop.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void calculateMultiples(int n) {
+        int i = 10;
+
+        while (i >= 1) {
+            System.out.print(i + " ");
+            i--;
+        }
+    }
+}
