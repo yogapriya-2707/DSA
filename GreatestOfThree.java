437d0f628db95d27237c0709a7284f63e7d2436e
Yogapriya.G <gyogapriya2707@gmail.com>
2026-03-21 21:57:47 +0530

    Add files via upload

---
+++ GreatestOfThree.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int greatestOfThree(int a, int b, int c) {
+        if (a >= b && a >= c) {
+            return a;
+        }
+        else if (b >= a && b >= c) {
+            return b;
+        }
+        else {
+            return c;
+        }
+    }
+}
