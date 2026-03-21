5e4eb9310b3a9b49f422abe2ceb911636a8a429a
Yogapriya.G <gyogapriya2707@gmail.com>
2026-03-21 20:14:20 +0530

    Add files via upload

---
+++ ReverseDigit.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reverseDigits(int n) {
+        int reverse = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            reverse = reverse * 10 + digit;
+            n = n / 10;
+        }
+        return reverse;
+    }
+}
