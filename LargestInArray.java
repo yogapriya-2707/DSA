cb2ceb33cdc0c4b4c22321dc31a1c3582f8d1b19
Yogapriya.G <gyogapriya2707@gmail.com>
2026-04-10 11:00:32 +0530

    Update LargestInArray.java

--- LargestInArray.java
+++ LargestInArray.java
@@ -1,13 +1,31 @@
-class Solution {
-    public static int largest(int[] arr) {
-        int n = arr.length;
-        int i;
-        int large = 0;
-        for(i=0 ; i<n ; i++){
-            if(arr[i]>large){
-                large = arr[i];
-            }
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] num = new int[n];
+        System.out.println("Enter elements : ");
+        for (int i = 0; i < n; i++) {
+            num[i] = sc.nextInt();
         }
-        return large;
+        int result = s.largest(num);
+        System.out.println("Largest Number : ");
+        System.out.println(result);
+    }
+
+    static class Solution {
+        public static int largest(int[] arr) {
+            int n = arr.length;
+            int i;
+            int large = 0;
+            for(i=0 ; i<n ; i++){
+                if(arr[i]>large){
+                    large = arr[i];
+                }
+            }
+            return large;
+        }
     }
 }
