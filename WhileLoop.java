92de5e40a02c50be8826d5a85724062c1bda939c
Yogapriya.G <gyogapriya2707@gmail.com>
2026-04-10 10:49:22 +0530

    Update WhileLoop.java

--- WhileLoop.java
+++ WhileLoop.java
@@ -1,10 +1,21 @@
-class Solution {
-    public void calculateMultiples(int n) {
-        int i = 10;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number : ");
+        int num = sc.nextInt();
+        s.calculateMultiples(num);
+    }
 
-        while (i >= 1) {
-            System.out.print(n*i + " ");
-            i--;
+    static class Solution {
+        public void calculateMultiples(int n) {
+            int i = 10;
+
+            while (i >= 1) {
+                System.out.print(n * i + " ");
+                i--;
+            }
         }
     }
 }
