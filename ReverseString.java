3e257d3ff53bad647cf30d30bd6133f56c109b48
Yogapriya.G <gyogapriya2707@gmail.com>
2026-04-10 10:34:52 +0530

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -1,9 +1,21 @@
-class Solution {
-    static String revStr(String s) {
-        String rev = "";
-        for (int i = s.length() - 1; i >= 0; i--) {
-            rev = rev + s.charAt(i);
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string : ");
+        String str = sc.nextLine();
+        String rev = s.revStr(str);
+        System.out.println("Reversed string : ");
+        System.out.println(rev);
+    }
+    static class Solution {
+        static String revStr(String s) {
+            String rev = "";
+            for (int i = s.length() - 1; i >= 0; i--) {
+                rev = rev + s.charAt(i);
+            }
+            return rev;
         }
-        return rev;
     }
 }
