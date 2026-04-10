edbe8b1d16033d1e9fe68b31bb2cb98ebd5a9b80
Yogapriya.G <gyogapriya2707@gmail.com>
2026-04-10 11:05:51 +0530

    Update InputOutput.java

--- InputOutput.java
+++ InputOutput.java
@@ -1,8 +1,21 @@
-class Solution {
-    static void printIntString(String S, int N) {
-        System.out.print("The input string :");
-        System.out.println(S);
-        System.out.print("The input integer :");
-        System.out.println(N);
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string : ");
+        String S = sc.nextLine();
+        System.out.println("Enter integer : ");
+        int N = sc.nextInt();
+        s.printIntString(S,N);
+    }
+
+    static class Solution {
+        static void printIntString(String S, int N) {
+            System.out.print("The input string :");
+            System.out.println(S);
+            System.out.print("The input integer :");
+            System.out.println(N);
+        }
     }
 }
