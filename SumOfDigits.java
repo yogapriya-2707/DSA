943995e5756eb209ff50eb82967e12db8cbb2489
Yogapriya.G <gyogapriya2707@gmail.com>
2026-04-10 10:44:07 +0530

    Update SumOfDigits.java

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -1,13 +1,22 @@
-class Solution {
-    static int sumOfDigits(int n) {
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            int num = i;
-            while (num > 0) {
-                sum += num % 10;
-                num /= 10;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter digit : ");
+        int num = sc.nextInt();
+        int sum = s.sumOfDigits(num);
+        System.out.println("Sum of digits : ");
+        System.out.println(sum);
+    }
+    static class Solution {
+        static int sumOfDigits(int n) {
+            int sum = 0;
+            while (n > 0) {
+                sum += n % 10;
+                n /= 10;
             }
+            return sum;
         }
-        return sum;
     }
 }
