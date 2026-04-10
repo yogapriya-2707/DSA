851cdbb45e8d48e933ff2d9853cefc76ef5a29a5
Yogapriya.G <gyogapriya2707@gmail.com>
2026-04-10 11:53:47 +0530

    Update ForLoop.java

--- ForLoop.java
+++ ForLoop.java
@@ -1,14 +1,26 @@
-class Solution {
-    public static String isPrime(int n) {
-        if(n<=1){
-            return "No";
-        }
-        int i;
-        for(i=2;i<n;i++){
-            if(n%i == 0){
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number : ");
+        int n = sc.nextInt();
+        String result = s.isPrime(n);
+        System.out.println(result);
+    }
+
+    static class Solution {
+        public static String isPrime(int n) {
+            if(n<=1){
                 return "No";
             }
+            int i;
+            for(i=2;i<n;i++){
+                if(n%i == 0){
+                    return "No";
+                }
+            }
+            return "Yes";
         }
-        return "Yes";
     }
 }
