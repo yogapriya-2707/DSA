707d70cb7ee8908699bdb026cc83ad3ee9c7a4ba
Yogapriya.G <gyogapriya2707@gmail.com>
2026-04-10 11:12:58 +0530

    Update IfElseIf.java

--- IfElseIf.java
+++ IfElseIf.java
@@ -1,13 +1,28 @@
-class Solution {
-    public static String compareNM(int n, int m) {
-        if(n < m){
-            return "lesser";
-        }
-        else if(n == m){
-            return "equal";
-        }
-        else{
-            return "greater";
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value for n: ");
+        int n = sc.nextInt();
+        System.out.println("Enter value for m : ");
+        int m = sc.nextInt();
+        String result = s.compareNM(n,m);
+        System.out.println("Result : ");
+        System.out.println(result);
+    }
+
+    static class Solution {
+        public static String compareNM(int n, int m) {
+            if(n < m){
+                return "lesser";
+            }
+            else if(n == m){
+                return "equal";
+            }
+            else{
+                return "greater";
+            }
         }
     }
 }
