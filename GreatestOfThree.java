19a8844ce42f1c47c13ce25de4dd304d04706aa7
Yogapriya.G <gyogapriya2707@gmail.com>
2026-04-10 11:48:18 +0530

    Update GreatestOfThree.java

--- GreatestOfThree.java
+++ GreatestOfThree.java
@@ -1,13 +1,32 @@
-class Solution {
-    public int greatestOfThree(int a, int b, int c) {
-        if (a >= b && a >= c) {
-            return a;
-        }
-        else if (b >= a && b >= c) {
-            return b;
-        }
-        else {
-            return c;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value for a : ");
+        int a = sc.nextInt();
+        System.out.println("Enter value for b : ");
+        int b = sc.nextInt();
+        System.out.println("Enter value for c : ");
+        int c = sc.nextInt();
+        s.greatestOfThree(a,b,c);
+    }
+
+    static class Solution {
+        public int greatestOfThree(int a, int b, int c) {
+            if (a >= b && a >= c) {
+                System.out.print(a);
+                System.out.println(" is greatest among three");
+            }
+            else if (b >= a && b >= c) {
+                System.out.print(b);
+                System.out.println(" is greatest among three");
+            }
+            else {
+                System.out.print(c);
+                System.out.println(" is greatest among three");
+            }
+            return 0;
         }
     }
 }
