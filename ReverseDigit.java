b5dad143e7e0a50468ac7ba977c9a71b38c43ef7
Yogapriya.G <gyogapriya2707@gmail.com>
2026-04-10 10:28:57 +0530

    Update ReverseDigit.java

--- ReverseDigit.java
+++ ReverseDigit.java
@@ -1,11 +1,23 @@
-class Solution {
-    public int reverseDigits(int n) {
-        int reverse = 0;
-        while (n > 0) {
-            int digit = n % 10;
-            reverse = reverse * 10 + digit;
-            n = n / 10;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number : ");
+        int number = sc.nextInt();
+        int reverse = s.reverseDigits(number);
+        System.out.println("Reversed digit : ");
+        System.out.println(reverse);
+    }
+    static class Solution {
+        public int reverseDigits(int n) {
+            int reverse = 0;
+            while (n > 0) {
+                int digit = n % 10;
+                reverse = reverse * 10 + digit;
+                n = n / 10;
+            }
+            return reverse;
         }
-        return reverse;
     }
 }
