8009cb40a7ff93a473073a9ec9b713b63facef2d
Yogapriya.G <gyogapriya2707@gmail.com>
2026-04-22 09:34:54 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Solution s = new Solution();
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        s.selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static class Solution {
+        void selectionSort(int[] arr) {
+            int n = arr.length;
+            for (int i = 0; i < n - 1; i++) {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++) {
+                    if (arr[j] < arr[minIndex]) {
+                        minIndex = j;
+                    }
+                }
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+}
