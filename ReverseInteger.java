d0b04db8baab4dc7a794a9fd6a0484633dd3f4a1
Yogapriya.G <gyogapriya2707@gmail.com>
2026-04-29 10:14:52 +0530

    Add files via upload

---
+++ ReverseInteger.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ReverseInteger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Solution s = new Solution();
+        System.out.println("Enter number : ");
+        int num = sc.nextInt();
+        int result = s.reverse(num);
+        System.out.println(result);
+    }
+    static class Solution {
+        public int reverse(int x) {
+            boolean sign = true;
+            if(x<0){
+                sign = false;
+            }
+            x = Math.abs(x);
+            long rev = 0;
+            while(x>0){
+                rev = (rev*10)+(x%10);
+                x = x/10;
+                if(rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE){
+                    return 0;
+                }
+            }
+            if(!sign){
+                return -1*(int)rev;
+            }
+            return (int)rev;
+        }
+    }
+}
